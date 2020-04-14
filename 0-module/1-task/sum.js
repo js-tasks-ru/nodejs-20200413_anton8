0e5fe42338424f2fb53875e22b3cac40b236fe4d
Anton Dorozhkin <tropikan4@gmail.com>
2020-04-14 05:55:30 +0300

    01: sum

--- 0-module/1-task/sum.js
+++ 0-module/1-task/sum.js
@@ -1,5 +1,12 @@
+function isNumber(value) {
+  return typeof value === 'number' && !isNaN(value);
+}
+
 function sum(a, b) {
-  /* ваш код */
+  if (!isNumber(a) || !isNumber(b)) {
+    throw new TypeError();
+  }
+  return a + b;
 }
 
 module.exports = sum;
